4705c0db9fdc3e53b85625e1e4011d8c5770a9f9
tomoya <200378857@student.georgianc.on.ca>
2019-06-17 13:15:21 -0400

    Fix 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import useHero from "../hooks/useHero";
+import background from '../images/barrie_snow.jpg'
 
-const NotFoundPage = () => (
+const NotFoundPage = () => {
+
+  const {updateBackground, updateHeaders,updatePage} = useHero()
+  const headers = {header: 'NOT FOUND', subheader: 'You just hit a route that doesn&#39;t exist... the sadness.'};
+
+  useEffect(() => {
+      updateBackground(background);
+      updateHeaders(headers)
+      updatePage('/404/')
+  },);
+return (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
+}
 
 export default NotFoundPage
